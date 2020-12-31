05eb03f5214283f04ac2bbfea294f1aa2a0e9dd5
predator2508 <citroen8897@gmail.com>
2020-12-31 15:31:47 +0200

    1 hw

---
+++ lecon_un/10_12_5.py
@@ -0,0 +1,14 @@
+# пятое упражнение
+# площадь и периметр прямоугольного треугольника
+print('\nпятое упражнение')
+
+katet_un = float(input('Введите длину первого катета: '))
+# тип данных float, что бы расширить возможности для входных данных
+katet_deux = float(input('Введите длину второго катета: '))
+
+gipotenouse = (katet_un ** 2 + katet_deux ** 2) ** 0.5
+square_tr = (katet_un * katet_deux) / 2
+perimetr_tr = katet_un + katet_deux + gipotenouse
+
+print('Площадь заданного треугольника равна:', square_tr, 'кв. ед.'
+      '\nПериметр заданного треугольника равен:', perimetr_tr, 'ед.')
